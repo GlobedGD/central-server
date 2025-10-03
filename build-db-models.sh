8900cecdeaae4138c64103a31a357bce357d0225
dankmeme01 <42031238+dankmeme01@users.noreply.github.com>
2025-10-03 02:13:41 +0200

    updates rcipt

--- build-db-models.sh
+++ build-db-models.sh
@@ -4,3 +4,4 @@
 
 rm -rf src/users/database/entities
 DATABASE_URL=sqlite://db.sqlite sea-orm-cli generate entity -o src/users/database/entities
+DATABASE_URL=sqlite://features.sqlite sea-orm-cli generate entity -o src/features/database/entities
