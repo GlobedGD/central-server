b4e8e0f3257391adf8fb1e09d06dcc70244677f1
dankmeme01 <42031238+dankmeme01@users.noreply.github.com>
2026-03-03 02:05:19 +0100

    make user_id expire in 7 days

--- src/analytics/migrations/0001_initial.sql
+++ src/analytics/migrations/0001_initial.sql
@@ -1,6 +1,8 @@
 CREATE TABLE login_events (
     timestamp DateTime64(3) DEFAULT now(),
-    user_id Int32,
+    -- since user ids are potentially identifiable info, we store them for a much shorter duration than the rest of the table
+    -- we still retain them temporarily to prevent abuse and stuff
+    user_id Int32 TTL timestamp + INTERVAL 7 DAY,
     ip_address IPv6,
     connection_type LowCardinality(String),
     globed_version LowCardinality(String),
