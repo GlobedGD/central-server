a11c6828c8285e4c516f9ff3a2f6f60e58789434
dankmeme01 <42031238+dankmeme01@users.noreply.github.com>
2026-03-06 20:52:27 +0100

    fix stalls when using udp to connect and using admin panel

--- src/core/handler/admin.rs
+++ src/core/handler/admin.rs
@@ -710,40 +710,50 @@ impl ConnectionHandler {
     ) -> HandlerResult<()> {
         must_admin_auth(client)?;
 
-        let users = self.module::<UsersModule>();
+        // spawn a task for this, to not block the client for too long
+        let client = client.clone();
+        let server = self.server();
+        let r#type = r#type.to_owned();
 
-        let (logs, users) =
-            match users.admin_fetch_logs(issuer, target, r#type, before, after, page).await {
-                Ok(x) => x,
-                Err(e) => {
-                    self.send_admin_db_result(client, Err(e))?;
-                    return Ok(());
+        tokio::spawn(async move {
+            let this = server.handler();
+            let users = this.module::<UsersModule>();
+
+            let (logs, users) =
+                match users.admin_fetch_logs(issuer, target, &r#type, before, after, page).await {
+                    Ok(x) => x,
+                    Err(e) => {
+                        let _ = this.send_admin_db_result(&client, Err(e));
+                        return;
+                    }
+                };
+
+            let Ok(buf) = data::encode_message_dyn!(this, msg => {
+                let mut msg = msg.reborrow().init_admin_logs_response();
+
+                let mut accounts = msg.reborrow().init_accounts(users.len() as u32);
+                for (i, user) in users.iter().enumerate() {
+                    let acc = accounts.reborrow().get(i as u32);
+                    Self::encode_account_data(user, acc);
                 }
+
+                let mut out_logs = msg.init_logs(logs.len() as u32);
+                for (i, log) in logs.iter().enumerate() {
+                    let mut out = out_logs.reborrow().get(i as u32);
+                    out.set_id(log.id);
+                    out.set_account_id(log.account_id);
+                    out.set_target_account_id(log.target_account_id.unwrap_or(0));
+                    out.set_type(&log.r#type);
+                    out.set_timestamp(log.timestamp);
+                    out.set_expires_at(log.expires_at.unwrap_or(0));
+                    out.set_message(log.message.as_deref().unwrap_or_default());
+                }
+            }) else {
+                return;
             };
 
-        let buf = data::encode_message_dyn!(self, msg => {
-            let mut msg = msg.reborrow().init_admin_logs_response();
-
-            let mut accounts = msg.reborrow().init_accounts(users.len() as u32);
-            for (i, user) in users.iter().enumerate() {
-                let acc = accounts.reborrow().get(i as u32);
-                Self::encode_account_data(user, acc);
-            }
-
-            let mut out_logs = msg.init_logs(logs.len() as u32);
-            for (i, log) in logs.iter().enumerate() {
-                let mut out = out_logs.reborrow().get(i as u32);
-                out.set_id(log.id);
-                out.set_account_id(log.account_id);
-                out.set_target_account_id(log.target_account_id.unwrap_or(0));
-                out.set_type(&log.r#type);
-                out.set_timestamp(log.timestamp);
-                out.set_expires_at(log.expires_at.unwrap_or(0));
-                out.set_message(log.message.as_deref().unwrap_or_default());
-            }
-        })?;
-
-        client.send_data_bufkind(buf);
+            client.send_data_bufkind(buf);
+        });
 
         Ok(())
     }
@@ -751,32 +761,41 @@ impl ConnectionHandler {
     pub async fn handle_admin_fetch_mods(&self, client: &ClientStateHandle) -> HandlerResult<()> {
         must_admin_auth(client)?;
 
-        let users = self.module::<UsersModule>();
+        // spawn a task for this, to not block the client for too long
+        let client = client.clone();
+        let server = self.server();
 
-        let users = match users.fetch_moderators().await {
-            Ok(x) => x,
-            Err(e) => {
-                self.send_admin_db_result(client, Err(e))?;
-                return Ok(());
-            }
-        };
+        tokio::spawn(async move {
+            let this = server.handler();
+            let users = this.module::<UsersModule>();
 
-        let buf = data::encode_message_dyn!(self, msg => {
-            let mut resp = msg.init_admin_fetch_mods_response();
-            let mut ser = resp.reborrow().init_users(users.len() as u32);
+            let users = match users.fetch_moderators().await {
+                Ok(x) => x,
+                Err(e) => {
+                    let _ = this.send_admin_db_result(&client, Err(e));
+                    return;
+                }
+            };
 
-            for (i, user) in users.iter().enumerate() {
-                let mut u = ser.reborrow().get(i as u32);
-                u.set_account_id(user.account_id);
-                u.set_username(&user.username);
-                u.set_cube(user.cube);
-                u.set_color1(user.color1);
-                u.set_color2(user.color2);
-                u.set_glow_color(user.glow_color);
-            }
-        })?;
+            let Ok(buf) = data::encode_message_dyn!(this, msg => {
+                let mut resp = msg.init_admin_fetch_mods_response();
+                let mut ser = resp.reborrow().init_users(users.len() as u32);
 
-        client.send_data_bufkind(buf);
+                for (i, user) in users.iter().enumerate() {
+                    let mut u = ser.reborrow().get(i as u32);
+                    u.set_account_id(user.account_id);
+                    u.set_username(&user.username);
+                    u.set_cube(user.cube);
+                    u.set_color1(user.color1);
+                    u.set_color2(user.color2);
+                    u.set_glow_color(user.glow_color);
+                }
+            }) else {
+                return;
+            };
+
+            client.send_data_bufkind(buf);
+        });
 
         Ok(())
     }
